5069203e5293375a43d3a064887f1ac449a92fbb
u1810291 <otabekbutcher@gmail.com>
2020-12-17 15:12:36 +0500

    ICONS UPDATED, I DID THAT BAYBE ;-D

--- src/components/Sidebar/Body/index.jsx
+++ src/components/Sidebar/Body/index.jsx
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 import { Container, Space } from "./style";
 import Item from "../Item";
 import { childRoutes } from "../../../routes/sidebar-elemets";
+import logo from "../../../assets/icons/logo3.svg";
 
 export const SidebarBody = ({ collapsed }) => (
   <Container>
+    <img src={logo} />
     <Space />
     {childRoutes.map(
       ({ title, icon, path, children, except }, index) =>
--- src/components/Sidebar/Body/style.js
+++ src/components/Sidebar/Body/style.js
@@ -1,7 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div``;
 
+export const ImageWrapper = styled.div``;
+
 export const Space = styled.div`
-    height: 34px;
- `;
+  height: 34px;
+`;
