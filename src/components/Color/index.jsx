463baef4110b462ca9fdffad367587146eb12e34
u1810291 <otabekbutcher@gmail.com>
2020-12-18 08:59:37 +0500

    finished colors

--- src/components/Color/index.jsx
+++ src/components/Color/index.jsx
@@ -7,6 +7,21 @@ export default () => {
       <Card color="#E73E28" />
       <Card color="#F4F7FC" />
       <Card color="#F3F6FB" />
+      <Card color="#FF974A" />
+      <Card color="#FC5A5A" />
+      <Card color="#82C43C" />
+      <Card color="#A461D8" />
+      <Card color="#FF9AD5" />
+      <Card color="#171725" />
+      <Card color="#44444F" />
+      <Card color="#696974" />
+      <Card color="#92929D" />
+      <Card color="#B5B5BE" />
+      <Card color="#D4D4DC" />
+      <Card color="#E2E2EA" />
+      <Card color="#F1F1F5" />
+      <Card color="#FAFAFB" />
+      <Card color="#FFFFFF" />
     </Container>
   );
 };
--- src/components/Color/style.js
+++ src/components/Color/style.js
@@ -4,5 +4,5 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 20px;
-  position: relative;
+  width: 96%;
 `;
