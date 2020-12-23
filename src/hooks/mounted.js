3a96fb38f7cdf625d11b4f386794925ed7c06679
u1810291 <otabekbutcher@gmail.com>
2020-12-23 09:12:07 +0500

    promise hook

--- src/hooks/mounted.js
+++ src/hooks/mounted.js
@@ -10,3 +10,29 @@ export const useMounted = () => {
   });
   return useCallback(() => mounted.current, []);
 };
+
+export const useAsync = () => {
+  const isMounted = useMounted();
+  return useCallback(
+    (promise, onCancle) =>
+      new Promise((resolve, reject) => {
+        promise
+          .then((result) => {
+            if (isMounted()) {
+              resolve(result);
+            }
+          })
+          .catch((error) => {
+            if (isMounted()) {
+              reject(error);
+            }
+          })
+          .finally(() => {
+            if (!isMounted() && onCancle) {
+              onCancle();
+            }
+          });
+      }),
+    [isMounted]
+  );
+};
