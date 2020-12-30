da97230e47de62f5afad3e8fc0edc01dccc37c59
u1810291 <otabekbutcher@gmail.com>
2020-12-30 18:47:48 +0500

    separated

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,93 +1,12 @@
 import React, {useMemo} from "react";
-import styled from "styled-components";
-import {useTable, useExpanded} from "react-table";
-
+import {Container} from "./style";
 import makeData from "./makeData";
-
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`;
-
-function Table({columns: userColumns, data}) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    state: {expanded},
-  } = useTable(
-    {
-      columns: userColumns,
-      data,
-    },
-    useExpanded // Use the useExpanded plugin hook
-  );
-
-  return (
-    <>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <br />
-      <div>Showing the first 20 results of {rows.length} rows</div>
-      <pre>
-        <code>{JSON.stringify({expanded: expanded}, null, 2)}</code>
-      </pre>
-    </>
-  );
-}
 
 export default () => {
   const columns = useMemo(
     () => [
       {
-        id: "expander", // Make sure it has an ID
+        id: "expander",
         Header: ({getToggleAllRowsExpandedProps, isAllRowsExpanded}) => (
           <span {...getToggleAllRowsExpandedProps()}>{isAllRowsExpanded ? "👇" : "👉"}</span>
         ),
@@ -145,8 +64,8 @@ export default () => {
   const data = useMemo(() => makeData(5, 5, 5), []);
 
   return (
-    <Styles>
+    <Container>
       <Table columns={columns} data={data} />
-    </Styles>
+    </Container>
   );
 };
