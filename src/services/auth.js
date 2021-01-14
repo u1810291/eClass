0531d68a6ba7d309aa86354499ca0a6037f35923
u1810291 <o.rizakulov@student.inha.uz>
2021-01-14 19:34:31 +0500

    some

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,10 @@
-import axios, {execute, service} from ".";
-import {authSelector} from "./selectors";
+import axios, { execute, service } from '.';
+import { authSelector } from './selectors';
+
 export default {
-  getToken: ({username, password}) => authSelector(service.post("/login", {username, password})),
-  sendRestLink: (data) => execute(axios.post("/auth/reset", {data})),
-  authVerify: (params) => execute(axios.post("/front-end/auth/verify", {params})),
-  changePassword: (data) => execute(axios.post("/auth/changePassword"), {data}),
-  refreshToken: ({refresh_token}) => authSelector(service.post("/refresh", {refresh_token})),
+  getToken: ({ username, password }) => authSelector(service.post('/api/v1/login', { username, password })),
+  sendRestLink: (data) => execute(axios.post('/auth/reset', { data })),
+  authVerify: (params) => execute(axios.post('/front-end/auth/verify', { params })),
+  changePassword: (data) => execute(axios.post('/auth/changePassword'), { data }),
+  refreshToken: ({ refreshToken }) => authSelector(service.post('/refresh', { refreshToken })),
 };
