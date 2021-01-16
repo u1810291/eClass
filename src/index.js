dc988df4de170e5a8784d301db30b05d6e78241c
u1810291 <o.rizakulov@student.inha.uz>
2021-01-16 20:15:51 +0500

    fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./@fake-api";
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en";
-import Root from "./root";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './@fake-api';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import Root from './root';
+import * as serviceWorker from './serviceWorker';
 
 TimeAgo.addDefaultLocale(en);
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();
