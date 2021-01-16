d351e8947d769be14dea015ce8b394d1a0492b64
u1810291 <o.rizakulov@student.inha.uz>
2021-01-16 22:01:48 +0500

    some tiny f*cking changes

--- src/@fake-api/auth.js
+++ src/@fake-api/auth.js
@@ -9,7 +9,7 @@ export default (mock) => {
         return resolve([200, { data: { access_token: 'test_token', refresh_token: 'userId' } }]);
       }
       return resolve([400, { message: 'not matched' }]);
-    }),
+    })
   );
   mock.onPost('/auth/reset').reply(200, { message: 'success' });
   mock.onPost('/front-end/auth/verify').reply(200, { message: 'success' });
