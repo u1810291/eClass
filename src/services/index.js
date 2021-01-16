4d099443a72bfe87d20392edfb2918aad19e0e24
u1810291 <o.rizakulov@student.inha.uz>
2021-01-17 02:13:41 +0500

    some updates

--- src/services/index.js
+++ src/services/index.js
@@ -17,6 +17,7 @@ const CustomAxios = {
 service.interceptors.response.use(
   (res) => res,
   (error) => {
+    console.log(error);
     if (error.response.status === 401) {
       window.location.replace('/logout');
       localStorage.removeItem('token');
