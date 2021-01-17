c862a87521c17debde0193a84edda7c2c6741010
u1810291 <o.rizakulov@student.inha.uz>
2021-01-17 23:46:42 +0500

    some

--- src/routes/sidebar-routes.js
+++ src/routes/sidebar-routes.js
@@ -1,3 +1,4 @@
+import types from '../constants/action-types';
 import Examples from '../components/Examples';
 import Courses from '../views/Student/Courses';
 import Balance from '../views/Balance';
@@ -13,14 +14,40 @@ import Connection from '../views/Student/Connection';
 import Schedule from '../views/Teacher/Shedule';
 import Chat from '../views/Chat';
 import Error from '../components/Error';
+/*
 
+'CONFIRM_STUDENT',
+
+'VIEW_RIGHTS_LIST',
+'VIEW_ACCESS_RIGHTS_OF_ANY_USER',
+'CREATE_ACCESS_RIGHT',
+'UPDATE_ACCESS_RIGHT',
+'DELETE_ACCESS_RIGHT',
+'ASSIGN_ACCESS_RIGHT',
+'DEASSIGN_ACCESS_RIGHT',
+'CREATE_LIST_ITEM',
+'UPDATE_LIST_ITEM',
+'DELETE_LIST_ITEM',
+'TOPUP_ACCOUNT',
+'VIEW_ANY_TRANSACTION',
+'VIEW_ANY_ACCOUNT',
+
+'UPLOAD_MATERIALS_FOR_ANY_TARGET',
+'UPDATE_ANY_MATERIALS',
+'DELETE_ANY_MATERIALS',
+
+*/
 export const childRoutes = [
   {
     path: '/profile',
     component: Profile,
     icon: 'profile2',
     title: 'Profile',
-    permissions: [],
+    permissions: [
+      types.UPDATE_ANY_STUDENT_PROFILE,
+      types.UPDATE_ANY_TEACHER_PROFILE,
+      types.UPDATE_ANY_ADMIN_PROFILE
+    ],
     children: []
   },
   {
@@ -123,7 +150,24 @@ export const childRoutes = [
     component: Chat,
     icon: 'chat2',
     title: 'Chat',
-    permissions: [],
+    permissions: [
+      types.SEND_PERSONAL_MESSAGE,
+      types.SEND_ROOM_MESSAGE,
+      types.UPDATE_OWN_MESSAGE,
+      types.DELETE_OWN_MESSAGE,
+      types.CREATE_CHAT_ROOM,
+      types.UPDATE_OWN_CHAT_ROOM,
+      types.DELETE_OWN_CHAT_ROOM,
+      types.VIEW_MEMBERS_IN_ANY_ROOM,
+      types.ADD_USER_TO_ANY_ROOM,
+      types.ADD_GROUP_TO_ANY_ROOM,
+      types.ADD_USER_TO_MEMBER_ROOM,
+      types.ADD_GROUP_TO_MEMBER_ROOM,
+      types.DELETE_USER_FROM_ANY_ROOM,
+      types.DELETE_GROUP_FROM_ANY_ROOM,
+      types.DELETE_USER_FROM_MEMBER_ROOM,
+      types.DELETE_GROUP_FROM_MEMBER_ROOM
+    ],
 
     children: []
   },
@@ -144,61 +188,124 @@ export const childRoutes = [
     permissions: [],
 
     children: []
+  },
+  {
+    path: '/group',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Group',
+    permissions: [
+      types.VIEW_OWN_GROUP,
+      types.VIEW_ANY_GROUP,
+      types.CREATE_GROUP,
+      types.UPDATE_GROUP,
+      types.DELETE_GROUP,
+      types.VIEW_ALL_GROUPS_LIST,
+      types.VIEW_STUDENTS_IN_ANY_GROUP,
+      types.ADD_STUDENTS_TO_ANY_GROUP,
+      types.DELETE_STUDENTS_FROM_ANY_GROUP,
+      types.VIEW_ANY_PHOTO,
+      types.UPDATE_ANY_PHOTO,
+      types.DELETE_ANY_PHOTO,
+      types.UPLOAD_OWN_PHOTO,
+      types.DELETE_OWN_PHOTO,
+      types.UPLOAD_TEACHER_CERTIFICATE,
+      types.DELETE_TEACHER_CERTIFICATE,
+      types.VIEW_ALL_FILES_LIST,
+      types.UPLOAD_ANY_FILE,
+      types.VIEW_ANY_FILE,
+      types.DELETE_ANY_FILE
+    ],
+    children: []
+  },
+  {
+    path: '/subject',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Subject',
+    permissions: [
+      types.VIEW_SUBJECTS_LIST,
+      types.VIEW_ANY_SUBJECT,
+      types.CREATE_SUBJECT,
+      types.UPDATE_SUBJECT,
+      types.DELETE_SUBJECT
+    ],
+
+    children: []
+  },
+  {
+    path: '/logs',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Logs',
+    permissions: [
+      types.VIEW_OWN_LOGS,
+      types.VIEW_ANONYMOUS_LOGS,
+      types.VIEW_ANY_LOGS],
+
+    children: []
+  },
+  {
+    path: '/users',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Users',
+    permissions: [
+      types.REGISTER_STUDENT,
+      types.REGISTER_TEACHER,
+      types.REGISTER_ADMIN,
+      types.UPDATE_STUDENT,
+      types.UPDATE_TEACHER,
+      types.UPDATE_ADMIN,
+      types.DELETE_STUDENT,
+      types.DELETE_TEACHER,
+      types.DELETE_ADMIN,
+      types.UPDATE_OWN_PROFILE,
+      types.VIEW_ALL_STUDENTS_LIST,
+      types.VIEW_ALL_TEACHERS_LIST,
+      types.VIEW_ALL_ADMINS_LIST,
+      types.VIEW_STUDENT_PROFILE,
+      types.VIEW_TEACHER_PROFILE,
+      types.VIEW_ADMIN_PROFILE
+    ],
+
+    children: []
+  },
+  {
+    path: '/tariff',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Tariff',
+    permissions: [
+      types.VIEW_TARIFFS_LIST,
+      types.VIEW_ANY_TARIFF,
+      types.CREATE_TARIFF,
+      types.UPDATE_TARIFF,
+      types.DELETE_TARIFF
+    ],
+
+    children: []
+  },
+  {
+    path: '/lessons',
+    component: Examples,
+    icon: 'profile2',
+    title: 'Lessons',
+    permissions: [
+      types.VIEW_ANY_LESSONS_LIST,
+      types.CREATE_LESSON,
+      types.UPDATE_LESSON,
+      types.DELETE_LESSON,
+      types.CANCEL_ANY_LESSON,
+      types.RESCHEDULE_LESSON,
+      types.RESCHEDULE_ANY_LESSON,
+      types.START_ANY_LESSON,
+      types.FINISH_ANY_LESSON,
+      types.VIEW_ANY_LESSONS_MATERIALS
+    ],
+
+    children: []
   }
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
-  // {
-  //   path: '/examples',
-  //   component: Examples,
-  //   icon: 'profile2',
-  //   title: 'Examples',
-  //   permissions: [],
-
-  //   children: []
-  // },
   // {
   //   path: '/examples',
   //   component: Examples,
