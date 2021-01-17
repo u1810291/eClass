20bde62de185470053a8a917f0b41058057dd7f8
u1810291 <o.rizakulov@student.inha.uz>
2021-01-17 23:08:00 +0500

    some

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -5,6 +5,7 @@ import Profile from '../../components/Profile';
 export default () => {
   const { userData } = useSelector((state) => state.userReducer);
 
+  // eslint-disable-next-line no-console
   console.log(userData);
   return (
     <>
