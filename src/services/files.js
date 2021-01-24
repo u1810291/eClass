02ecab87ff0327528b9093833d24826a9093ebcf
u1810291 <o.rizakulov@student.inha.uz>
2021-01-24 19:25:09 +0500

    files

---
+++ src/services/files.js
@@ -0,0 +1,9 @@
+import { service } from '.';
+
+export default {
+  getAll: () => service.get('/api/v1/files/list'),
+  uploadFile: ({ data }) => service.post('/api/v1/files/upload', { data }),
+  downloadFile: ({ id }) => service.get(`/api/v1/files/download/${id}`),
+  deleteFile: ({ id }) => service.delete(`/api/v1/files/download/${id}`)
+
+};
