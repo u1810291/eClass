dbaa261e8172dba939e7ff338e0af29ff657d7c6
u1810291 <o.rizakulov@student.inha.uz>
2021-01-24 21:10:25 +0500

    lessons & profiles

--- src/services/admin/lesson.js
+++ src/services/admin/lesson.js
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import { service } from '..';
-import { authSelector } from '../selectors';
 
 export default {
-  getAdminLessons: () => authSelector(service.get('/api/v1/lessons/list'))
+  getLessons: () => service.get('/api/v1/lessons/list')
 };
