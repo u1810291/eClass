698e687c618f8bf94c5fa28843e2c268eab9e707
u1810291 <o.rizakulov@student.inha.uz>
2021-02-07 20:15:02 +0500

    initial commit in this branch

--- src/views/Homeworks/index.jsx
+++ src/views/Homeworks/index.jsx
@@ -1,3 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default () => <div>Homeworks</div>;
+import { useSelector, useDispatch } from 'react-redux';
+import Table from '../../components/Table';
+import { Container } from './style';
+import { FIVEPLUSADMIN, STUDENT, TEACHER } from '../../constants/roles';
+import { fetchData as student } from '../../redux/modules/student/lessons/actions';
+import { fetchData as teacher } from '../../redux/modules/teacher/lessons/actions';
+import { fetchData as admin } from '../../redux/modules/admin/lessons/actions';
+import { adminHeader, teacherHeader, studentHeader } from './helper';
+
+const AdminPage = ({ userInfo }) => {
+  const dispatch = useDispatch();
+
+  const { data } = useSelector((state) => state.adminLessonsReducers);
+  useEffect(() => {
+    dispatch(admin());
+  }, [admin]);
+
+  return (
+    <Container>
+      <Table perms={userInfo.rights} data={data} header={adminHeader} />
+    </Container>
+  );
+};
+const StudentPage = ({ userInfo }) => {
+  const dispatch = useDispatch();
+
+  const { data } = useSelector((state) => state.studentLessonsReducers);
+  useEffect(() => {
+    dispatch(student());
+  }, [student]);
+
+  return (
+    <Container>
+      <Table perms={userInfo.rights} data={data} header={studentHeader} />
+    </Container>
+  );
+};
+
+const TeacherPage = ({ userInfo }) => {
+  const dispatch = useDispatch();
+
+  const { data } = useSelector((state) => state.teacherLessonsReducers);
+
+  useEffect(() => {
+    dispatch(teacher());
+  }, [teacher]);
+
+  return (
+    <Container>
+      <Table perms={userInfo.rights} data={data} header={teacherHeader} />
+    </Container>
+  );
+};
+export default () => {
+  const { userInfo } = useSelector((state) => state.userReducer);
+
+  if (userInfo.role === FIVEPLUSADMIN) {
+    return (
+      <AdminPage
+        userInfo={userInfo}
+      />
+    );
+  }
+  if (userInfo.role === TEACHER) {
+    return (
+      <TeacherPage
+        userInfo={userInfo}
+      />
+    );
+  }
+  if (userInfo.role === STUDENT) {
+    return (
+      <StudentPage
+        userInfo={userInfo}
+      />
+    );
+  }
+  return <></>;
+};
