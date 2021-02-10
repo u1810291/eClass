4c94419c320b8421706ef6a924d6f1831c3b0f5c
u1810291 <o.rizakulov@student.inha.uz>
2021-02-10 23:45:43 +0500

    notification service finished

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -2,5 +2,5 @@ import { service } from '.';
 
 export default {
   getNotifications: () => service.get('/api/v1/notifications'),
-  getNotification: ({ id }) => service.get(`/api/v1/notifications/read/${id}`)
+  readNotification: ({ id }) => service.post(`/api/v1/notifications/read/${id}`)
 };
