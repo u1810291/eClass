b0022fd77379cc522ec473c3788587de90cefd49
u1810291 <o.rizakulov@student.inha.uz>
2021-02-12 19:45:11 +0500

    calendar almost done

--- src/components/Attendance/index.jsx
+++ src/components/Attendance/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import Calendar from '../CustomCalendar';
 import { Container, BigCalendar } from './style';
+import Card from '../Card';
 
 export default () => {
   const events = [
@@ -15,11 +16,12 @@ export default () => {
   ];
   return (
     <Container>
-      Attendance page
       <BigCalendar>
-        <Calendar
-          events={events}
-        />
+        <Card>
+          <Calendar
+            events={events}
+          />
+        </Card>
       </BigCalendar>
     </Container>
   );
--- src/components/Attendance/style.js
+++ src/components/Attendance/style.js
@@ -5,4 +5,9 @@ export const Container = styled.div`
 `;
 
 export const BigCalendar = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 10px;
+  margin-top: 10px;
+  grid-template-columns: 3fr 1fr;
 `;
