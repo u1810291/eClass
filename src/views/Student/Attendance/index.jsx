b4af2c43b85bf0951da9b8ea23ee69ca2d3ad1c8
u1810291 <o.rizakulov@student.inha.uz>
2021-02-12 18:04:01 +0500

    Attendance header

--- src/views/Student/Attendance/index.jsx
+++ src/views/Student/Attendance/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Attendance from '../../../components/Attendance';
+import AttendanceHeader from '../../../components/Headers/AttendanceHeader';
 import { Container } from './style';
 
 export default () => (
   <Container>
+    <AttendanceHeader />
     <Attendance />
   </Container>
 );
