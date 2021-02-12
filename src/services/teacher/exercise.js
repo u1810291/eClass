552a9f0ffdcfec74194bcb1f7d0eb5b0c97ec4dc
u1810291 <o.rizakulov@student.inha.uz>
2021-02-12 12:01:05 +0500

    quizes initialize

--- src/services/teacher/exercise.js
+++ src/services/teacher/exercise.js
@@ -3,7 +3,7 @@ import { service } from '..';
 export default {
 
   getHomeworks: (user) => service.get(`/api/v1/${user}/exercise/homework/list`),
-  getQuizes: (id) => service.get(`/api/v1/teacher/exercise/controlwork/list?lesson=${id}`),
+  getQuizes: (user) => service.get(`/api/v1/${user}/exercise/controlwork/list`),
   addHomework: (id, data) => service.post(`/api/v1/teacher/exercise/homework/${id}`, { data }),
   addQuize: (id, data) => service.post(`/api/v1/teacher/exercise/controlwork/${id}`, { data }),
   // TODO Needs to be checked before using
