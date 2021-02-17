6b652f9e2480e2247c4aeefd14c811bb0eaf3444
u1810291 <o.rizakulov@student.inha.uz>
2021-02-17 23:59:58 +0500

    another

--- src/components/Library/index.jsx
+++ src/components/Library/index.jsx
@@ -1,19 +1,36 @@
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
-import { Container } from './style';
+import { Container, Content } from './style';
 import Card from '../Card';
+import Icon from '../Icon';
 
 export default () => (
   <Container>
     <Card size="small">
-
-      1 Library
+      <Content>
+        <Content.Header>
+          1 Library
+        </Content.Header>
+        {
+          [...Array(12)].map((_, i) => (
+            <Content.Item key={i}>
+              <Content.Icon />
+              <Content.ItemBody>
+                <Content.ItemTop>English</Content.ItemTop>
+                <Content.ItemBottom>Subjeect</Content.ItemBottom>
+                <Content.Cancel>
+                  <Icon icon="cross" size="5px" />
+                </Content.Cancel>
+              </Content.ItemBody>
+            </Content.Item>
+          ))
+        }
+      </Content>
     </Card>
     <Card size="small">
-
       2 Library
     </Card>
     <Card size="small">
-
       3 Library
     </Card>
   </Container>
--- src/components/Library/style.js
+++ src/components/Library/style.js
@@ -4,5 +4,18 @@ export const Container = styled.div`
   display: grid;
   grid-gap: 10px;
   grid-template-columns: 1fr 1fr 1fr;
-
+  padding: 10px;
 `;
+
+export const Content = styled.div`
+  displa
+`;
+
+Content.Header = styled.span``;
+
+Content.Item = styled.div``;
+Content.Icon = styled()``;
+Content.ItemBody = styled.div``;
+Content.ItemTop = styled.span``;
+Content.ItemBottom = styled.span``;
+Content.Cancel = styled.div``;
