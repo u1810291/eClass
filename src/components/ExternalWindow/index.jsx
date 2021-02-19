0160ebf5c03ba25ad5aebdfda489f5c65fc52700
u1810291 <o.rizakulov@student.inha.uz>
2021-02-19 15:24:28 +0500

    some errors

--- src/components/ExternalWindow/index.jsx
+++ src/components/ExternalWindow/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const ExternalWindow = (props) => {
