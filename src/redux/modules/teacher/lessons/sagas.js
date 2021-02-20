e28e352f2d37a08612385f81413d22a0776374af
u1810291 <o.rizakulov@student.inha.uz>
2021-02-20 09:57:12 +0500

    teacher lessons

---
+++ src/redux/modules/teacher/lessons/sagas.js
@@ -0,0 +1,32 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import types from '../../../constants/action-types';
+import service from '../../../services/teacher/lesson';
+import {
+  setData,
+  setError,
+  setTotal,
+  setLoading
+} from './actions';
+
+import { dataSelector } from './selectors';
+
+function* fetchData({ payload }) {
+  try {
+    const { user, query } = payload;
+    if (user) {
+      const role = user.toLowerCase();
+      const res = yield service.getAll(role, query);
+      const { total, data } = dataSelector(res.data);
+      yield put(setError(''));
+      yield put(setData(data));
+      yield put(setTotal(total));
+      yield put(setLoading(false));
+    }
+  } catch (error) {
+    yield put(setError(error.message));
+  }
+}
+
+export default function* lessonsSaga() {
+  yield takeLatest(types.TABLE_LESSONS_FETCH_DATA, fetchData);
+}
