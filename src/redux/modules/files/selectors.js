84334b98c9a7f6a2d74b7070b590b8a9460ead85
u1810291 <o.rizakulov@student.inha.uz>
2021-02-25 23:27:42 +0500

    minor changes

--- src/redux/modules/files/selectors.js
+++ src/redux/modules/files/selectors.js
@@ -1,19 +1,6 @@
-import moment from 'moment';
-
-export function dataSelector(lesson) {
+export function dataSelector(data) {
   // eslint-disable-next-line camelcase
-  const { total_elements, content } = lesson;
-  const filtered = content.map((el) => ({
-    date: moment(el.scheduled_start).format('DD.MM.YYYY'),
-    time: moment(el.scheduled_start).format('hh:mm:ss'),
-    duration: el.duration,
-    teacher: {
-      name: el.teacher.full_name,
-      role: 'Teacher',
-      image: null
-    },
-    subject: el.subject.name,
-    link: { join: el.join_url }
-  }));
-  return { total: total_elements, data: filtered };
+  const filtered = data;
+  console.log(filtered);
+  return { data: filtered };
 }
