5bf3e4549f03b07c54066195561097441fd66bbb
u1810291 <o.rizakulov@student.inha.uz>
2021-02-28 23:02:47 +0500

    some

--- src/views/Admin/Groups/helper.js
+++ src/views/Admin/Groups/helper.js
@@ -1,24 +1,31 @@
 /* eslint-disable no-alert */
 export const toolTips = [
   {
+    name: 'Create lesson',
+    icon: 'payment',
+    onClick: () => {
+      alert('Add lesson');
+    }
+  },
+  {
     name: 'Delete group',
     icon: 'payment',
     onClick: () => {
-      alert('Response Reschedule');
+      alert('Delete group');
     }
   },
   {
     name: 'Add student',
     icon: 'payment',
     onClick: () => {
-      alert('Cancel');
+      alert('Add student');
     }
   },
   {
     name: 'Delete student',
     icon: 'payment',
     onClick: () => {
-      alert('Reschedule');
+      alert('Delete student');
     }
   }
 ];
