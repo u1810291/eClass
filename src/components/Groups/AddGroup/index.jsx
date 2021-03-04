67fc6d3f7f946511c66f0ed472c3531c7f71a62a
u1810291 <o.rizakulov@student.inha.uz>
2021-03-04 07:18:41 +0500

    some

--- src/components/Groups/AddGroup/index.jsx
+++ src/components/Groups/AddGroup/index.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
-import { Container } from './style';
+import {
+  Container, Body, Header, Footer, Main
+} from './style';
+import Card from '../../Card';
 
 export default () => (
-  <Container />
+  <Container>
+    <Card>
+      <Body>
+        <Header>
+          Header
+        </Header>
+        <Main>
+          Main
+        </Main>
+        <Footer>
+          Footer
+        </Footer>
+      </Body>
+    </Card>
+  </Container>
 );
--- src/components/Groups/AddGroup/style.js
+++ src/components/Groups/AddGroup/style.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Body = styled.div`
+  display:grid;
+  grid-gap:10px;
+  grid-template-areas: 
+      header
+      main
+      footer
+`;
+export const Header = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-area: header;
+`;
+export const Main = styled.div`
+  display:grid;
+  grid-gap: 10px;
+  grid-area: main;
+  grid-template-columns:1fr 1fr;
+  `;
+export const Footer = styled.div`
+  display:grid;
+  grid-gap: 10px;
+  grid-area: main;
+  grid-template-columns:1fr 1fr 1fr;
+`;
