e145a85d8d873ab908459692a0142df331040c02
u1810291 <o.rizakulov@student.inha.uz>
2021-03-04 07:30:31 +0500

    tiny

--- src/components/Groups/AddGroup/index.jsx
+++ src/components/Groups/AddGroup/index.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Container, Body, Header, Footer, Main
 } from './style';
 import { NormalInput } from '../../Forms/Inputs';
+import Dropdown from '../../Forms/Dropdowns';
+import { dropdownOptions } from '../../../data/dropdown';
+
 import Card from '../../Card';
 
-export default () => (
-  <Container>
-    <Card>
-      <Body>
-        <Header>
-          <NormalInput placeholder="Group name" />
-          <NormalInput placeholder="Official name" />
-        </Header>
-        <Main>
-          Main
-        </Main>
-        <Footer>
-          Footer
-        </Footer>
-      </Body>
-    </Card>
-  </Container>
-);
+export default () => {
+  const [commonDropdown, setCommonDropdown] = useState(undefined);
+  return (
+    <Container>
+      <Card>
+        <Body>
+          <Header>
+            <NormalInput placeholder="Group name" />
+            <NormalInput placeholder="Official name" />
+            <Dropdown
+              placeholder="Select"
+              options={dropdownOptions.commonOptions}
+              value={commonDropdown}
+              onChange={setCommonDropdown}
+              size="large"
+            />
+          </Header>
+          <Main>
+            <Dropdown
+              placeholder="Select"
+              options={dropdownOptions.commonOptions}
+              value={commonDropdown}
+              onChange={setCommonDropdown}
+              size="large"
+            />
+            <Dropdown
+              placeholder="Select"
+              options={dropdownOptions.commonOptions}
+              value={commonDropdown}
+              onChange={setCommonDropdown}
+              size="large"
+            />
+
+            <NormalInput placeholder="Percent for teacher" />
+            <NormalInput placeholder="Number of room" />
+            <NormalInput placeholder="Price for one lesson" />
+            <NormalInput label="Price for one lesson with discount" placeholder="Price for one lesson with discount" />
+          </Main>
+          <Footer>
+            Footer
+          </Footer>
+        </Body>
+      </Card>
+    </Container>
+  );
+};
