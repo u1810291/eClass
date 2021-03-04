25ba79d293ee7bc27193ad210ec2f5fa2ac412e0
u1810291 <o.rizakulov@student.inha.uz>
2021-03-04 07:23:21 +0500

    minnnnor

--- src/components/Groups/AddGroup/style.js
+++ src/components/Groups/AddGroup/style.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin: 20px;
+`;
 
 export const Body = styled.div`
+  padding: 20px;
   display:grid;
   grid-gap:10px;
-  grid-template-areas: 
-      header
-      main
-      footer
+  grid-template-areas: "header"
+                       "main"
+                       "footer"
 `;
 export const Header = styled.div`
   display: grid;
@@ -25,6 +27,6 @@ export const Main = styled.div`
 export const Footer = styled.div`
   display:grid;
   grid-gap: 10px;
-  grid-area: main;
+  grid-area: footer;
   grid-template-columns:1fr 1fr 1fr;
 `;
--- src/views/Admin/Groups/AddGroup/index.jsx
+++ src/views/Admin/Groups/AddGroup/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container } from '../style';
+import AddGroup from '../../../../components/Groups/AddGroup';
 
 export default () => (
   <Container>
-    Add Group
+    <AddGroup />
   </Container>
 );
