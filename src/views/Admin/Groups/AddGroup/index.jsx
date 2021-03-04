8b77aff5827b8482d1118a7dd06a3c5ba9ca3a24
u1810291 <o.rizakulov@student.inha.uz>
2021-03-04 16:29:47 +0500

    some

--- src/views/Admin/Groups/AddGroup/index.jsx
+++ src/views/Admin/Groups/AddGroup/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+
 import { Container } from '../style';
 import AddGroup from '../../../../components/Groups/AddGroup';
+import { usingFormik } from './helper';
 
-export default () => (
-  <Container>
-    <AddGroup />
-  </Container>
-);
+export default () => {
+  const formik = usingFormik();
+  return (
+    <Container>
+      <AddGroup formik={formik} />
+    </Container>
+  );
+};
