d634766e56d44b35045af34cf0cda665a12cdb0a
u1810291 <o.rizakulov@student.inha.uz>
2021-03-22 05:52:06 +0500

    some changes

--- src/components/MeetingWindow/index.jsx
+++ src/components/MeetingWindow/index.jsx
@@ -3,11 +3,14 @@
 import React, { useEffect, useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { ZoomMtg } from '@zoomus/websdk';
 import { useHistory } from 'react-router-dom';
 import { Container } from './style';
 import { teacherSign } from '../../redux/modules/lessons/actions';
+// eslint-disable-next-line
+declare var ZoomMtg;
+
 // import { PrimaryButton } from '../Buttons';
+
 ZoomMtg.setZoomJSLib('https://source.zoom.us/1.9.1/lib', '/av');
 
 ZoomMtg.preLoadWasm();
@@ -18,13 +21,14 @@ export default ({ data }) => {
   useEffect(() => {
     dispatch(teacherSign(data));
   }, []);
+  console.log(teacherData);
 
   const apiKey = teacherData.api_key;
   const meetingNumber = teacherData.meeting_id;
   const leaveUrl = `${window.location.pathname}?ROLE=${1}&USERNAME=${'USERNAME'}`;
   const userName = 'Teacher';
   const userEmail = 'some@some.com';
-  const passWord = 690579;
+  const passWord = '284472';
 
   function startMeeting(signature) {
     document.getElementById('zmmtg-root').style.display = 'block';
@@ -56,14 +60,14 @@ export default ({ data }) => {
   }
   function getSignature(e) {
     e.preventDefault();
-    startMeeting(teacherData);
+    startMeeting(teacherData.signature);
   }
   return (
     <Container>
 
       <main>
         <h1>Zoom WebSDK Sample React</h1>
-
+        <div className="zmmtg-root"></div>
         <button type="button" onClick={getSignature}>Join Meeting</button>
       </main>
 
