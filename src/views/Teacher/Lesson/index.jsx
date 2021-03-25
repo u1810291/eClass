74365eb9b23051b67d0d8ef6910b20ab9fb6c212
u1810291 <o.rizakulov@student.inha.uz>
2021-03-25 07:03:57 +0500

    fixed

--- src/views/Teacher/Lesson/index.jsx
+++ src/views/Teacher/Lesson/index.jsx
@@ -10,8 +10,6 @@ import TableError from '../../../components/Table/Error';
 import { headerMaker } from '../../../components/Table/helper';
 import { toolTips } from './helper';
 
-
-
 export default () => {
   const dispatch = useDispatch();
   const [pageIndex, setPageIndex] = useState(0);
