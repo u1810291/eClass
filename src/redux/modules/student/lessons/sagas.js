5a93fb7504258d8661dcbcc76ff9aebff93157b7
u1810291 <o.rizakulov@student.inha.uz>
2021-03-29 07:43:05 +0500

    tiny changes

--- src/redux/modules/student/lessons/sagas.js
+++ src/redux/modules/student/lessons/sagas.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { takeLatest, put } from 'redux-saga/effects';
 import types from '../../../../constants/action-types';
 import service from '../../../../services/student/lesson';
