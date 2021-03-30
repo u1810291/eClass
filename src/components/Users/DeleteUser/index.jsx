f79c2759e622e1de18000c01fe8b54589f40aca4
u1810291 <o.rizakulov@student.inha.uz>
2021-03-30 17:56:53 +0500

    some

--- src/components/Users/DeleteUser/index.jsx
+++ src/components/Users/DeleteUser/index.jsx
@@ -5,7 +5,6 @@ import { Container } from './style';
 export default ({ id, useDelete }) => {
   const data = { user: 'student', id };
   const { token } = useDelete(data);
-  console.log(token);
   return (
     <Container>
       {token}
