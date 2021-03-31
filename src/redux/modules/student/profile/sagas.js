dae86a4590d50a3f5026053b8f746044807a7e6f
u1810291 <o.rizakulov@student.inha.uz>
2021-03-31 08:07:00 +0500

    minor changes

--- src/redux/modules/student/profile/sagas.js
+++ src/redux/modules/student/profile/sagas.js
@@ -1,6 +1,6 @@
 import { takeLatest, put } from 'redux-saga/effects';
 import types from '../../../../constants/action-types';
-import service from '../../../../services/student/exercise';
+import service from '../../../../services/student/profile';
 import {
   setData,
   setError,
@@ -10,10 +10,10 @@ import {
 
 import { dataSelector } from './selectors';
 
-function* fetchData({ payload }) {
+function* fetchData() {
   yield put(setLoading(true));
   try {
-    const res = yield service.getAll(payload.query);
+    const res = yield service.getProfile();
     const { total, data } = dataSelector(res.data);
     yield put(setError(''));
     yield put(setData(data));
@@ -24,6 +24,6 @@ function* fetchData({ payload }) {
   }
 }
 
-export default function* studentHomeworksSaga() {
+export default function* studentProfileSaga() {
   yield takeLatest(types.STUDENT_PROFILE_FETCH_DATA, fetchData);
 }
