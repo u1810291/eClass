c2a505308833a3b1fbcdd5a9e5a097c99c342fc6
u1810291 <o.rizakulov@student.inha.uz>
2021-04-01 17:42:34 +0500

    some

--- src/views/Student/Balance/index.jsx
+++ src/views/Student/Balance/index.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 import {
   Container
 } from './style';
 import Balance from '../../../components/Balance';
+import { fetchData } from '../../../redux/modules/student/balance/actions';
 
 export default () => {
   const [date, setDate] = useState('');
+  const dispatch = useDispatch();
+  const { data, error, loading } = useSelector((state) => state.studentBalanceReducers);
   useEffect(() => {
+    dispatch(fetchData());
     setDate(moment(new Date()).format('DD-MM-YYYY hh:mm:ss'));
-  }, [date, setDate]);
+  }, [fetchData]);
+  console.log(data, error, loading);
   return (
     <Container>
       <Balance date={date} />
