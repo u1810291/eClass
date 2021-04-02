bdf3c1e8bbeeea16359fe26b76c8d16fbc4ae7d9
u1810291 <o.rizakulov@student.inha.uz>
2021-04-02 20:18:16 +0500

    SOME

--- src/components/Payments/index.jsx
+++ src/components/Payments/index.jsx
@@ -25,82 +25,79 @@ import Payme from '../../assets/icons/payme.svg';
 import Online from '../../assets/icons/online.svg';
 import Click from '../../assets/icons/click.svg';
 
-export default () => (
-  // console.log(data);
-  <Container>
-    <StepTwo>
-      <h3>Please choose one type of course</h3>
-      <Navigate.Cards>
-        <Wrapper className="scroll-container">
-          <InnerCard>
-            {[...Array(15)].map((_, i) => (
-              <MainCard key={i}>
-                <Card>
-                  <CardContent color={getRandColor()}>
-                    <Card>
-                      <Icon />
-                    </Card>
-                    <CardBody>
-                      <CardBody.Title>
-                        {i}
-                        {' '}
-                        title
-                      </CardBody.Title>
-                      <CardBody.CheckBox>
-                        <CheckBox />
-                      </CardBody.CheckBox>
-                      <CardBody.Value>
-                        550.000 sum
-                      </CardBody.Value>
-                    </CardBody>
-                  </CardContent>
-                </Card>
-              </MainCard>
-            ))}
-          </InnerCard>
-        </Wrapper>
-      </Navigate.Cards>
-    </StepTwo>
-    <Footer>
-      <StepThree>
-        <PaymentType>
-          Payment type
-          <PaymentType.List>
-            <PaymentType.Icon icon={Click} />
-            <PaymentType.Text>
-
-              Click
-            </PaymentType.Text>
-          </PaymentType.List>
-          <PaymentType.List>
-            <PaymentType.Icon icon={Payme} />
-            <PaymentType.Text>
-
-              Payme
-            </PaymentType.Text>
-          </PaymentType.List>
-          <PaymentType.List>
-            <PaymentType.Icon icon={Online} />
-            <PaymentType.Text>
-
-              Online
-            </PaymentType.Text>
-          </PaymentType.List>
-          <PaymentType.List>
-            <PaymentType.Icon icon={Cash} />
-            <PaymentType.Text>
-              Cash
-            </PaymentType.Text>
-          </PaymentType.List>
-        </PaymentType>
-      </StepThree>
-      <StepFour>
-        <Pay>
-
-          Some
-        </Pay>
-      </StepFour>
-    </Footer>
-
-  </Container>
-);
+export default ({ data }) => {
+  console.log(data);
+  return (
+    <Container>
+      <StepTwo>
+        <h3>Please choose one type of course</h3>
+        <Navigate.Cards>
+          <Wrapper className="scroll-container">
+            <InnerCard>
+              {[...Array(15)].map((_, i) => (
+                <MainCard key={i}>
+                  <Card>
+                    <CardContent color={getRandColor()}>
+                      <Card>
+                        <Icon />
+                      </Card>
+                      <CardBody>
+                        <CardBody.Title>
+                          {i}
+                          {' '}
+                          title
+                        </CardBody.Title>
+                        <CardBody.CheckBox>
+                          <CheckBox />
+                        </CardBody.CheckBox>
+                        <CardBody.Value>
+                          550.000 sum
+                        </CardBody.Value>
+                      </CardBody>
+                    </CardContent>
+                  </Card>
+                </MainCard>
+              ))}
+            </InnerCard>
+          </Wrapper>
+        </Navigate.Cards>
+      </StepTwo>
+      <Footer>
+        <StepThree>
+          <PaymentType>
+            Payment type
+            <PaymentType.List>
+              <PaymentType.Icon icon={Click} />
+              <PaymentType.Text>
+                Click
+              </PaymentType.Text>
+            </PaymentType.List>
+            <PaymentType.List>
+              <PaymentType.Icon icon={Payme} />
+              <PaymentType.Text>
+                Payme
+              </PaymentType.Text>
+            </PaymentType.List>
+            <PaymentType.List>
+              <PaymentType.Icon icon={Online} />
+              <PaymentType.Text>
+                Online
+              </PaymentType.Text>
+            </PaymentType.List>
+            <PaymentType.List>
+              <PaymentType.Icon icon={Cash} />
+              <PaymentType.Text>
+                Cash
+              </PaymentType.Text>
+            </PaymentType.List>
+          </PaymentType>
+        </StepThree>
+        <StepFour>
+          <Pay>
+            Some
+          </Pay>
+        </StepFour>
+      </Footer>
+    </Container>
+  );
+};
