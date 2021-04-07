100837c2063b7d1120d3e6907bf2ea513d72b563
u1810291 <o.rizakulov@student.inha.uz>
2021-04-07 17:51:54 +0500

    tiny

--- src/views/Admin/Tariff/index.jsx
+++ src/views/Admin/Tariff/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { Container, Content } from './style';
 import Table from '../../../components/Table';
+import { fetchData } from '../../../redux/modules/admin/tariffs/actions';
 
 export default () => {
-  const data = '';
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [fetchData]);
+  const {
+    data, error, total, loading
+  } = useSelector((state) => state.adminTariffsReducers);
+  // eslint-disable-next-line no-console
+  console.log(error);
   return (
     <Container>
       <Content>
