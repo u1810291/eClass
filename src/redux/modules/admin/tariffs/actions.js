62611c85801f1c53f8c886e2267f578f9fbac65a
u1810291 <o.rizakulov@student.inha.uz>
2021-04-07 15:32:29 +0500

    tiny change

--- src/redux/modules/admin/tariffs/actions.js
+++ src/redux/modules/admin/tariffs/actions.js
@@ -1,22 +1,7 @@
 import types from '../../../../constants/action-types';
 
-export const setLoading = (payload) => ({
-  type: types.ADMIN_TARIFFS_SET_LOADING,
-  payload
-});
-export const setData = (payload) => ({
-  type: types.ADMIN_TARIFFS_SET_DATA,
-  payload
-});
-export const setTotal = (payload) => ({
-  type: types.ADMIN_TARIFFS_SET_TOTAL,
-  payload
-});
-export const setError = (payload) => ({
-  type: types.ADMIN_TARIFFS_ERROR,
-  payload
-});
-export const fetchData = (payload) => ({
-  type: types.ADMIN_TARIFFS_FETCH_DATA,
-  payload
-});
+export const setLoading = (payload) => ({ type: types.ADMIN_TARIFFS_SET_LOADING, payload });
+export const setData = (payload) => ({ type: types.ADMIN_TARIFFS_SET_DATA, payload });
+export const setTotal = (payload) => ({ type: types.ADMIN_TARIFFS_SET_TOTAL, payload });
+export const setError = (payload) => ({ type: types.ADMIN_TARIFFS_ERROR, payload });
+export const fetchData = (payload) => ({ type: types.ADMIN_TARIFFS_FETCH_DATA, payload });
