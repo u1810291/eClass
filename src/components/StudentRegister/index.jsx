e24882a2898ab6c9422e6ebe770d1db93cf0a46e
u1810291 <o.rizakulov@student.inha.uz>
2021-04-08 13:51:29 +0500

    tiny changes

--- src/components/StudentRegister/index.jsx
+++ src/components/StudentRegister/index.jsx
@@ -16,7 +16,6 @@ export default ({
 }) => (
   <Container>
     <Header className={classNames('heading-1')}>Create new Student</Header>
-    {console.log(single)}
     <SubmitForm onSubmit={(e) => {
       e.preventDefault();
       formik.handleSubmit();
