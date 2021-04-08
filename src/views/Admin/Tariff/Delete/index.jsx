05f7c5b23cc1131a24da86ffb8998d4b696cd10f
u1810291 <o.rizakulov@student.inha.uz>
2021-04-08 16:12:49 +0500

    tiny changes

--- src/views/Admin/Tariff/Delete/index.jsx
+++ src/views/Admin/Tariff/Delete/index.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 export default (props) => {
   // eslint-disable-next-line no-unused-vars
   const dispatch = useDispatch();
+  // eslint-disable-next-line no-console
   console.log(props);
   return (
     <div>
--- src/views/Admin/Tariff/Update/index.jsx
+++ src/views/Admin/Tariff/Update/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default ({ id, useUpdateForm }) => {
   const { formik } = useUpdateForm(id);
+  // eslint-disable-next-line no-console
   console.log(formik);
   return (
     <div>
