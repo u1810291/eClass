269434eb790401f63afadef872fc83f80b5fbad1
u1810291 <o.rizakulov@student.inha.uz>
2021-04-09 19:43:20 +0500

    some

--- src/utils/generate-intials.js
+++ src/utils/generate-intials.js
@@ -1,16 +1,4 @@
 export const getInitials = (name, delimeter = ' ') => {
-  // if (name) {
-  //   const array = name.split(delimeter);
-
-  //   switch (array.length) {
-  //   case 1:
-  //     return array[0].charAt(0).toUpperCase();
-  //   default:
-  //     return array[0].charAt(0).toUpperCase() + array[array.length - 1].charAt(0).toUpperCase();
-  //   }
-  // }
-  // return false;
-
   const array = name.split(delimeter);
   let str = '';
 
