304a5948bf03b39b4c6ac277518b8f151c15369c
u1810291 <o.rizakulov@student.inha.uz>
2021-04-13 08:46:37 +0500

    SOME CHANGES

--- src/redux/modules/teacher/balance/sagas.js
+++ src/redux/modules/teacher/balance/sagas.js
@@ -15,7 +15,6 @@ function* fetchData() {
   yield put(setLoading(true));
   try {
     const res = yield service.getBalance();
-    console.log(res);
     const { total, data } = dataSelector(res.data);
     yield put(setError(''));
     yield put(setData(data));
