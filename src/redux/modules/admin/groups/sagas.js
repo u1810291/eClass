48e43ce2e623b0a02d486f20922b22aae966fe94
u1810291 <o.rizakulov@student.inha.uz>
2021-04-19 15:00:00 +0500

    minor change

--- src/redux/modules/admin/groups/sagas.js
+++ src/redux/modules/admin/groups/sagas.js
@@ -64,6 +64,7 @@ function* addGroup({ payload, success }) {
 
 function* addStudent({ payload, success }) {
   try {
+    console.log(payload);
     const res = yield service.addGroupStudents(payload);
     success(res);
   } catch (error) {
