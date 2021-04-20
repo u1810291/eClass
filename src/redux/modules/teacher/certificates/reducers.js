ed558fd65c040503958f3af90b3b65b92cbf2758
u1810291 <o.rizakulov@student.inha.uz>
2021-04-20 16:46:18 +0500

    certificates

---
+++ src/redux/modules/teacher/certificates/reducers.js
@@ -0,0 +1,25 @@
+import types from '../../../../constants/action-types';
+
+const defaultState = {
+  data: [],
+  loading: false,
+  error: ''
+};
+
+const map = {
+  [types.TEACHER_PROFILE_SET_LOADING]: (state, { payload }) => ({
+    ...state,
+    loading: payload
+  }),
+  [types.TEACHER_PROFILE_SET_DATA]: (state, { payload }) => ({
+    ...state,
+    data: payload
+  }),
+  [types.TEACHER_PROFILE_ERROR]: (state, { payload }) => ({
+    ...state,
+    error: payload
+  })
+};
+
+// eslint-disable-next-line max-len
+export default (state, action) => (map[action.type] && map[action.type](state, action)) || state || defaultState;
