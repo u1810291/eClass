b88d5a25a2d39eb26f38e1488c1194e36c1ca15e
u1810291 <o.rizakulov@student.inha.uz>
2021-04-20 11:16:18 +0500

    minor change

--- src/redux/modules/student/profile/actions.js
+++ src/redux/modules/student/profile/actions.js
@@ -4,3 +4,8 @@ export const setLoading = (payload) => ({ type: types.STUDENT_PROFILE_SET_LOADIN
 export const setData = (payload) => ({ type: types.STUDENT_PROFILE_SET_DATA, payload });
 export const setError = (payload) => ({ type: types.STUDENT_PROFILE_ERROR, payload });
 export const fetchData = () => ({ type: types.STUDENT_PROFILE_FETCH_DATA });
+
+export const updateProfile = (payload) => ({ type: types.STUDENT_PROFILE_UPDATE, payload });
+export const uploadPhoto = (payload) => ({ type: types.STUDENT_PROFILE_UPLOAD_PHOTO, payload });
+export const deletePhoto = (payload) => ({ type: types.STUDENT_PROFILE_DELETE_PHOTO, payload });
+export const downloadPhoto = (payload) => ({ type: types.STUDENT_PROFILE_DOWNLOAD_PHOTO, payload });
