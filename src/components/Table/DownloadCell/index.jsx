9d5900a2f56604fe60977765473c17450f712d43
u1810291 <o.rizakulov@student.inha.uz>
2021-04-22 16:41:02 +0500

    some

--- src/components/Table/DownloadCell/index.jsx
+++ src/components/Table/DownloadCell/index.jsx
@@ -9,16 +9,7 @@ const DownloadCell = (files) => (
         <button
           type="button"
           onClick={() => {
-            service.downloadExercise(files[0]).then((response) => {
-              const extension = response.headers['content-type'].split('/');
-              const url = window.URL.createObjectURL(new Blob([response.data]));
-              const link = document.createElement('a');
-              link.href = url;
-              link.setAttribute('download', `${files[0]}.${extension[1]}`); // or any other extension
-              document.body.appendChild(link);
-              link.click();
-              link.parentNode.removeChild(link);
-            });
+            service.downloadExercise(files[0]);
           }}
         >
           <Content size="large">
--- src/services/teacher/exercise.js
+++ src/services/teacher/exercise.js
@@ -10,5 +10,14 @@ export default {
 
   markExercise: ({ id, mark }) => service.post(`/api/v1/teacher/exercise/mark/${id}?mark=${mark}`),
   markExerciseToUploaded: ({ id, mark }) => service.post(`/api/v1/teacher/exercise/mark/${id}/student-new?mark=${mark}`)
-
+    .then((response) => {
+      const extension = response.headers['content-type'].split('/');
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', `${id}.${extension[1]}`); // or any other extension
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+    })
 };
