34542876ca79a608adf2beadf02e0f709ff40eeb
u1810291 <o.rizakulov@student.inha.uz>
2021-04-23 12:44:39 +0500

    redux

---
+++ src/redux/modules/student/quiz/actions.js
@@ -0,0 +1,25 @@
+import types from '../../../../constants/action-types';
+
+export const setLoading = (payload) => ({
+  type: types.TABLE_STUDENT_HOMEWORKS_SET_LOADING,
+  payload
+});
+export const setData = (payload) => ({ type: types.TABLE_STUDENT_HOMEWORKS_SET_DATA, payload });
+export const setTotal = (payload) => ({ type: types.TABLE_STUDENT_HOMEWORKS_SET_TOTAL, payload });
+export const setError = (payload) => ({ type: types.TABLE_STUDENT_HOMEWORKS_ERROR, payload });
+export const fetchData = (payload) => ({
+  type: types.TABLE_STUDENT_HOMEWORKS_FETCH_DATA,
+  payload
+});
+
+export const submitHomework = (payload, success) => ({
+  type: types.TABLE_STUDENT_HOMEWORKS_ADD,
+  payload,
+  success
+});
+
+export const updateHomework = (payload, success) => ({
+  type: types.TABLE_STUDENT_HOMEWORKS_UPDATE,
+  payload,
+  success
+});
---
+++ src/redux/modules/student/quiz/sagas.js
@@ -0,0 +1,55 @@
+import { takeLatest, put } from 'redux-saga/effects';
+import types from '../../../../constants/action-types';
+import service from '../../../../services/student/exercise';
+import {
+  setData,
+  setError,
+  setTotal,
+  setLoading
+} from './actions';
+
+import { dataSelector, submitHomeworkSelector } from './selectors';
+
+function* fetchData({ payload }) {
+  try {
+    yield put(setLoading(true));
+    const res = yield service.getAll(payload.query);
+    const { total, data } = dataSelector(res.data);
+    yield put(setError(''));
+    yield put(setData(data));
+    yield put(setTotal(total));
+    yield put(setLoading(false));
+  } catch (error) {
+    yield put(setError(error.response ? error.response.data.error_message : error));
+  }
+}
+
+function* submitHomework({ payload, success }) {
+  try {
+    const { data } = submitHomeworkSelector(payload.values);
+    const res = yield service.submitExercise(payload.id, data);
+    yield put(setError(''));
+    success(res);
+  } catch (error) {
+    // eslint-disable-next-line no-alert
+    alert(error);
+  }
+}
+
+function* updateHomework({ payload, success }) {
+  try {
+    const { data } = submitHomeworkSelector(payload.values);
+    const res = yield service.submitExercise(payload.id, data);
+    yield put(setError(''));
+    success(res);
+  } catch (error) {
+    // eslint-disable-next-line no-alert
+    alert(error);
+  }
+}
+
+export default function* studentHomeworksSaga() {
+  yield takeLatest(types.TABLE_STUDENT_HOMEWORKS_FETCH_DATA, fetchData);
+  yield takeLatest(types.TABLE_STUDENT_HOMEWORKS_ADD, submitHomework);
+  yield takeLatest(types.TABLE_STUDENT_HOMEWORKS_UPDATE, updateHomework);
+}
