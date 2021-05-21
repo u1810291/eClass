d87ac101e8f694da8af579b3cb5212e5ca0d618d
u1810291 <o.rizakulov@student.inha.uz>
2021-05-21 09:48:59 +0500

    minor change

--- src/services/chat.js
+++ src/services/chat.js
@@ -11,13 +11,13 @@ export default {
   deleteUserFromRoom: ({ id, uuid }) => service.delete(`/api/v1/chat/rooms/${id}/users/${uuid}`),
   addGroupToRoom: ({ id, uuid }) => service.post(`/api/v1/chat/rooms/${id}/groups/${uuid}`),
   deleteGroupFromRoom: ({ id, uuid }) => service.delete(`/api/v1/chat/rooms/${id}/groups/${uuid}`),
-  sendMessageToUser: ({ message, to_user }) => service.post('/api/v1/chat/message', { message, to_user }),
-  sendMessageToRoom: ({ message, to_room }) => service.post('/api/v1/chat/message', { message, to_room }),
   editMessage: ({ id }) => service.put(`/api/v1/chat/message/${id}`),
   deleteMessage: ({ id }) => service.delete(`/api/v1/chat/message/${id}`),
   getDialogs: ({ from_date, to_date }) => service.get('api/v1/chat/message/dialogs', { from_date, to_date }),
   // GET /chat/message/user/:user_id - Get messages exchange with a user
   // Get messages from/to specific user
+  sendMessageToUser: ({ message, to_user }) => service.post('/api/v1/chat/message', { message, to_user }),
+  sendMessageToRoom: ({ message, to_room }) => service.post('/api/v1/chat/message', { message, to_room }),
   getMessageOfUser: ({ id }) => service.get(`/api/v1/chat/message/user/${id}`),
   getMessageOfRoom: ({ id }) => service.get(`/api/v1/chat/message/room/${id}`)
 };
--- src/views/Chat/index.jsx
+++ src/views/Chat/index.jsx
@@ -1,4 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SockJS from 'sockjs-client'; // Note this line
+import Stomp from 'stompjs';
+import { useSelector } from 'react-redux';
 import Chat from '../../components/Chat';
+// import chat from '../../services/chat';
 
-export default () => <Chat />;
+export default () => {
+  const [state, setState] = useState('');
+  const jwt = localStorage.getItem('access_token');
+  const { userInfo } = useSelector((state) => state.userReducer);
+  function ChatScreen() {
+    useEffect(() => {
+      const socket = new SockJS('https://five-plus.co/ws');
+      const stompClient = Stomp.over(socket);
+      const headers = { Authorization: `Bearer ${jwt}` };
+      stompClient.connect(headers, () => {
+        stompClient.subscribe(
+          `/user/${userInfo.username}/v1/notification`, console.log, headers
+        );
+      });
+      return () => stompClient && stompClient.disconnect();
+    }, [jwt, userInfo.username]);
+  }
+  return (<Chat state={state} setState={setState}>{ChatScreen()}</Chat>);
+};
