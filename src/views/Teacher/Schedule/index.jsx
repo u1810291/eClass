7f480ddb410a1a1686869afeb7565bf077eceafa
u1810291 <o.rizakulov@student.inha.uz>
2021-05-25 13:30:54 +0500

    minor changes

--- src/views/Teacher/Schedule/index.jsx
+++ src/views/Teacher/Schedule/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Attendance from '../../../components/Attendance';
