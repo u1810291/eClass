7226dcd2e61367661acb60ac65b703a0ffde343a
u1810291 <o.rizakulov@student.inha.uz>
2021-05-30 19:56:43 +0500

    check again

--- src/redux/modules/student/profile/actions.js
+++ src/redux/modules/student/profile/actions.js
@@ -5,7 +5,7 @@ export const setData = (payload) => ({ type: types.STUDENT_PROFILE_SET_DATA, pay
 export const setError = (payload) => ({ type: types.STUDENT_PROFILE_ERROR, payload });
 export const fetchData = () => ({ type: types.STUDENT_PROFILE_FETCH_DATA });
 
-export const updateProfile = (payload) => ({ type: types.STUDENT_PROFILE_UPDATE, payload });
+export const updateProfile = (payload, success) => ({ type: types.STUDENT_PROFILE_UPDATE, payload });
 export const uploadPhoto = (payload) => ({ type: types.STUDENT_PROFILE_UPLOAD_PHOTO, payload });
 export const deletePhoto = (payload) => ({ type: types.STUDENT_PROFILE_DELETE_PHOTO, payload });
 export const downloadPhoto = (payload) => ({ type: types.STUDENT_PROFILE_DOWNLOAD_PHOTO, payload });
