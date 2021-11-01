3b923c224c786ef0635a559ee97159d35dc4bc51
Otabek <45333849+u1810291@users.noreply.github.com>
2021-11-01 15:16:16 +0500

    Delete clean_frontend_admin.sh

--- clean_frontend_admin.sh
+++
@@ -1,6 +0,0 @@
-docker ps -a --filter=name="fiveplus-frontend-main*"
-docker stop $(docker ps --filter=name="fiveplus-frontend-main*" -q)
-docker rm -v $(docker ps -a --filter=name="fiveplus-frontend-main*" -q)
-docker rmi $(docker images --filter=reference="fiveplus/frontend-main*" -q) -f
-
-read -n 1 -s -r -p "Press any key to continue"
